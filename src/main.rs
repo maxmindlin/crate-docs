c462d831d4e974b82554bec1947038065edf2677
Max Mindlin <maxmindlin@gmail.com>
2020-03-05 08:24:38 -0500

    init

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,60 @@ mod content;
 
 use std::io;
 use std::io::*;
+use std::process::Command;
 
 use content::*;
 
-enum Command {
+enum Cmd {
     Lookup(String),
-    Unknown
+    Unknown(String),
+    RefreshCache,
+    Empty,
+    InvalidUsage(String),
 }
 
-impl From<String> for Command {
+enum Allow {
+    Yes,
+    No,
+}
+
+impl From<&str> for Allow {
+    fn from(s: &str) -> Self {
+        match s {
+            "y" | 
+            "Y" |
+            "yes" |
+            "Yes" |
+            "YES" => Self::Yes,
+            _ => Self::No
+        }
+    }
+}
+
+impl From<String> for Allow {
+    fn from(s: String) -> Self {
+        Self::from(s.as_str())
+    }
+}
+
+impl From<String> for Cmd {
     fn from(s: String) -> Self {
         let cmds: Vec<&str> = s.split(" ").collect();
-        match cmds.len() {
-            2 => {
-                if cmds[0] == "lookup" {
-                    Self::Lookup(cmds[1].to_owned())
+
+        if cmds.len() == 0 {
+            return Self::Empty
+        }
+
+        match cmds[0] {
+            "lookup" => {
+                if cmds.len() != 2 {
+                    Self::InvalidUsage("lookup command must be length 2".to_owned())
                 } else {
-                    Self::Unknown
+                    Self::Lookup(cmds[1].to_owned())
                 }
             }
-            _ => Self::Unknown
+            "refresh" => Self::RefreshCache,
+            s => Self::Unknown(s.to_owned()),
         }
     }
 }
@@ -40,17 +74,33 @@ fn wait_for_cmd() -> String {
 fn main() {
     loop {
         let cmd = wait_for_cmd();
+        process_cmd(Cmd::from(cmd));
+    }
+}
 
-        match Command::from(cmd.to_owned()) {
-            Command::Unknown => println!("Unknown command"),
-            Command::Lookup(name) => {
-                let page = DocPage::fetch(false, &name);
-                match page {
-                    Err(e) => println!("{:?}", e),
-                    Ok(p) => p.print_tableview(),
-                }
+fn process_cmd(cmd: Cmd) {
+    match cmd {
+        Cmd::Empty => {},
+        Cmd::Unknown(s) => println!("Unknown command `{}`", s),
+        Cmd::Lookup(name) => {
+            let page = DocPage::fetch(false, &name);
+            match page {
+                Err(e) => println!("{:?}", e),
+                Ok(p) => p.print_tableview(),
             }
-        }
+        },
+        Cmd::InvalidUsage(s) => println!("Invalid command usage: {}", s),
+        Cmd::RefreshCache => {
+            println!("Refresh the doc cache? (y/n)");
+            println!("note: this may take a couple minutes depending on number of crates.");
+            let confirm = wait_for_cmd();
+            match Allow::from(confirm) {
+                Allow::Yes => {
+                    Command::new("cargo").arg("doc").spawn().expect("Failed to refresh docs");
+                },
+                Allow::No => return
+            }
+        },
     }
 }
 

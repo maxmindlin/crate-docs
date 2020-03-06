d4a1072db31a000413d5775ea738b42dd5dc8967
Max Mindlin <maxmindlin@gmail.com>
2020-03-05 21:37:01 -0500

    various changes

--- src/content.rs
+++ src/content.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::env;
 
+use url::Url;
 use reqwest;
 use scraper::{Html, Selector};
 use prettytable::{Table, Row, Cell};
@@ -20,12 +21,16 @@ pub enum PageType {
     Index(Html),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum DocType {
     Module,
     Struct,
     Type,
     Trait,
+    Enum,
+    Function,
+    Constant,
+    Other,
 }
 
 impl From<&DocType> for String {
@@ -35,26 +40,48 @@ impl From<&DocType> for String {
             DocType::Struct => "Structs".to_owned(),
             DocType::Type => "Types".to_owned(),
             DocType::Trait => "Traits".to_owned(),
+            DocType::Enum => "Enums".to_owned(),
+            DocType::Function => "Functions".to_owned(),
+            DocType::Constant => "Constants".to_owned(),
+            DocType::Other => "Others".to_owned(),
         }
     }
 }
 
 #[derive(Debug)]
 pub struct DocTypeListing {
-    doc_type: DocType,
-    docs: Vec<DocListing>,
+    pub doc_type: DocType,
+    pub docs: Vec<DocListing>,
+}
+
+impl Clone for DocTypeListing {
+    fn clone(&self) -> Self {
+        DocTypeListing {
+            doc_type: self.doc_type,
+            docs: self.docs.clone()
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct DocListing {
-    name: String,
-    url: String,
+    pub name: String,
+    pub url: String,
+}
+
+impl Clone for DocListing {
+    fn clone(&self) -> Self {
+        DocListing {
+            name: self.name.to_owned(),
+            url: self.url.to_owned(),
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct DocPage {
-    page_type: PageType,
-    doc_blocks: Vec<DocTypeListing>,
+    pub page_type: PageType,
+    pub doc_blocks: Vec<DocTypeListing>,
 }
 
 impl DocPage {
@@ -62,25 +89,15 @@ impl DocPage {
         let page = fetch_html(crate_name, online);
         match page {
             Err(e) => Err(e),
-            Ok(p) => {
-                let mut blocks: Vec<DocTypeListing> = Vec::new();
-                for dt in vec![
-                    DocType::Module,
-                    DocType::Struct,
-                    DocType::Type,
-                    DocType::Trait
-                ] {
-                    let listing = gen_doc_listing(&p, dt);
-                    match listing {
-                        Ok(Some(l)) => blocks.push(l),
-                        _ => continue
-                    }
+            Ok((p, u)) => {
+                let docs = gen_doc_listings(&p, &u);
+                match docs {
+                    Err(e) => Err(e),
+                    Ok(d) => Ok(Self {
+                        page_type: p,
+                        doc_blocks: d,
+                    })
                 }
-
-                Ok(Self {
-                    page_type: p,
-                    doc_blocks: blocks,
-                })
             }
         }
     }
@@ -104,50 +121,89 @@ impl DocPage {
     }
 }
 
-pub fn gen_doc_listing(page: &PageType, doc_type: DocType) -> Result<Option<DocTypeListing>, ContentError> {
+pub fn gen_doc_listings(page: &PageType, base_url: &str) -> Result<Vec<DocTypeListing>, ContentError> {
     match page {
         PageType::All(html) => {
-            let selector_str = match doc_type {
-                DocType::Module => ".modules.docblock",
-                DocType::Struct => ".structs.docblock",
-                DocType::Type => ".typedefs.docblock",
-                DocType::Trait => ".traits.docblock",
-            };
-            let selector = Selector::parse(selector_str).unwrap();
+            // On the All page the only ul nodes are those
+            // that belong to a grouping of listings, grouped
+            // under the same type.
+            let selector = Selector::parse("ul").unwrap();
+            let mut docs: Vec<DocTypeListing> = Vec::new();
+            for c in html.select(&selector) {
+                // Every doc component is listed as an anchor
+                // within their given docblock. Iterate through
+                // these and collect them into listing structs to 
+                // be attached to the set of all doc listings
+                // within the DocTypeListing struct.
 
-            let content = html.select(&selector).next();
-            match content {
-                // We didnt find anything that matches the 
-                // docblock selector. This _will_ happen pretty often
-                // as every crate does not export every type.
-                None => Ok(None),
-                Some(c) => {
-                    // Every doc component is listed as an anchor
-                    // within their given docblock. Iterate through
-                    // these and collect them into listing structs to 
-                    // be attached to the set of all doc listings
-                    // within the DocTypeListing struct.
-                    let mut listings: Vec<DocListing> = Vec::new();
-                    let entry_selector = Selector::parse("a").unwrap();
-                    for entry in c.select(&entry_selector) {
-                        let name = entry.text().collect::<String>();
-                        let url = match entry.value().attr("href") {
-                            Some(u) => u.to_owned(),
-                            None => "".to_owned(),
-                        };
-                        listings.push(DocListing {
-                            name: name,
-                            url: url,
-                        })
-                    }
+                let classes: Vec<&str> = match c.value().attr("class") {
+                    Some(c) => c.split(" ").collect(),
+                    None => Vec::new()
+                };
 
-                    Ok(Some(DocTypeListing {
-                        doc_type: doc_type,
+                // This node either doesnt have a class attr or 
+                // is a menu item, either way - dont want it.
+                if classes.len() == 0 || classes[0].starts_with("pure") {
+                    continue
+                }
+
+                let dtype = match classes[0] {
+                    "modules" => DocType::Module,
+                    "structs" => DocType::Struct,
+                    "typedefs" => DocType::Type,
+                    "traits" => DocType::Trait,
+                    "enums" => DocType::Enum,
+                    "functions" => DocType::Function,
+                    "constants" => DocType::Constant,
+                    _ => DocType::Other
+                };
+
+                let mut listings: Vec<DocListing> = Vec::new();
+                let entry_selector = Selector::parse("a").unwrap();
+                for entry in c.select(&entry_selector) {
+                    let name = entry.text().collect::<String>();
+                    let url = match entry.value().attr("href") {
+                        Some(u) => {
+                            match Url::parse(base_url) {
+                                Ok(base) => {
+                                    // These are easy, we have real URLs
+                                    base.join(u)
+                                        .unwrap()
+                                        .as_str()
+                                        .to_owned()
+                                },
+                                Err(url::ParseError::RelativeUrlWithoutBase) => {
+                                    // We should probably do this better,
+                                    // but this works for now.
+                                    // In here is when we have file locations,
+                                    // not urls. If we could parse file locations
+                                    // as urls this would be better.
+                                    base_url.replace("all.html", u).to_owned()
+                                },
+                                Err(_) => continue
+                            }
+                        },
+                        // if this anchor doesnt have an href (which should never happen?)
+                        // then we dont want it.
+                        None => continue,
+                    };
+                    listings.push(DocListing {
+                        name: name,
+                        url: url,
+                    })
+                }
+
+                // if its empty then we dont want it in the master set. That would
+                // result in empty tables.
+                if listings.len() > 0 {
+                    docs.push(DocTypeListing {
+                        doc_type: dtype,
                         docs: listings,
-                    }))
+                    });
                 }
             }
 
+            Ok(docs)
         },
         // We cannot fetch a doc listing page
         // from any pages that do not match the
@@ -156,7 +212,7 @@ pub fn gen_doc_listing(page: &PageType, doc_type: DocType) -> Result<Option<DocT
     }
 }
 
-pub fn fetch_live_html(crate_name: &str) -> Result<PageType, ContentError> {
+pub fn fetch_live_html(crate_name: &str) -> Result<(PageType, String), ContentError> {
     let url = format!("https://docs.rs/{}", crate_name);
     let resp = reqwest::blocking::get(&url);
     match resp {
@@ -171,10 +227,15 @@ pub fn fetch_live_html(crate_name: &str) -> Result<PageType, ContentError> {
             match resp {
                 Err(_) => Err(ContentError::LoadFailure),
                 Ok(r) => {
-                    let body = r.text();
-                    match body {
-                        Err(_) => Err(ContentError::LoadFailure),
-                        Ok(b) => Ok(PageType::All(Html::parse_document(&b)))
+                    match r.status() {
+                        reqwest::StatusCode::OK => {
+                            let body = r.text();
+                            match body {
+                                Err(_) => Err(ContentError::LoadFailure),
+                                Ok(b) => Ok((PageType::All(Html::parse_document(&b)), url))
+                            }
+                        },
+                        _ =>Err(ContentError::DoesNotExist),
                     }
                 }
             }
@@ -183,7 +244,7 @@ pub fn fetch_live_html(crate_name: &str) -> Result<PageType, ContentError> {
     }
 }
 
-pub fn fetch_html(crate_name: &str, online: bool) -> Result<PageType, ContentError> {
+pub fn fetch_html(crate_name: &str, online: bool) -> Result<(PageType, String), ContentError> {
     if online {
         return fetch_live_html(crate_name)
     }                                            
@@ -198,14 +259,14 @@ pub fn fetch_html(crate_name: &str, online: bool) -> Result<PageType, ContentErr
     // that lists all the exports. Attempt to
     // load it into an html page from the file content string.
     let index_path = format!("{}/target/doc/{}/all.html", path, crate_name);
-    let file = File::open(index_path);
+    let file = File::open(index_path.clone());
     match file {
         Err(_) => Err(ContentError::DoesNotExist),
         Ok(mut f) => {
             let mut content = String::new();
             match f.read_to_string(&mut content) {
                 Err(_) => Err(ContentError::LoadFailure),
-                Ok(_) => Ok(PageType::All(Html::parse_document(&content)))
+                Ok(_) => Ok((PageType::All(Html::parse_document(&content)), index_path))
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use] extern crate prettytable;
 mod content;
 
 use std::io;
@@ -7,7 +6,39 @@ use std::process::Command;
 
 use content::*;
 
+struct DocState {
+    page: DocPage,
+    available_docs: Vec<DocListing>,
+}
+
+impl From<DocPage> for DocState {
+    fn from(page: DocPage) -> Self {
+        let mut listings: Vec<DocListing> = Vec::new();
+        for block in page.doc_blocks.clone() {
+            listings.extend(block.docs.clone());
+        }
+
+        Self {
+            page: page,
+            available_docs: listings
+        }
+    }
+}
+
+impl DocState {
+    fn search_doc_listings(&self, target: &str) -> Option<DocListing> {
+        for doc in &self.available_docs {
+            if &doc.name == target || doc.name.ends_with(target) {
+                return Some(doc.clone())
+            }
+        }
+
+        None
+    }
+}
+
 enum Cmd {
+    Doc(String),
     Lookup(String),
     Unknown(String),
     RefreshCache,
@@ -43,26 +74,33 @@ impl From<String> for Cmd {
     fn from(s: String) -> Self {
         let cmds: Vec<&str> = s.split(" ").collect();
 
-        if cmds.len() == 0 {
+        if cmds == vec![""] {
             return Self::Empty
         }
 
         match cmds[0] {
-            "lookup" => {
+            "lup" => {
                 if cmds.len() != 2 {
                     Self::InvalidUsage("lookup command must be length 2".to_owned())
                 } else {
                     Self::Lookup(cmds[1].to_owned())
                 }
             }
-            "refresh" => Self::RefreshCache,
+            "rc" => Self::RefreshCache,
+            "doc" => {
+                if cmds.len() != 2 {
+                    Self::InvalidUsage("doc command must be length 2".to_owned())
+                } else {
+                    Self::Doc(cmds[1].to_owned())
+                }
+            }
             s => Self::Unknown(s.to_owned()),
         }
     }
 }
 
-fn wait_for_cmd() -> String {
-    print!(">> ");
+fn wait_for_cmd(prompt: &str) -> String {
+    print!("{} ", prompt);
     io::stdout().flush().unwrap();
 
     let mut cmd = String::new();
@@ -73,7 +111,7 @@ fn wait_for_cmd() -> String {
 
 fn main() {
     loop {
-        let cmd = wait_for_cmd();
+        let cmd = wait_for_cmd(">>");
         process_cmd(Cmd::from(cmd));
     }
 }
@@ -82,25 +120,71 @@ fn process_cmd(cmd: Cmd) {
     match cmd {
         Cmd::Empty => {},
         Cmd::Unknown(s) => println!("Unknown command `{}`", s),
-        Cmd::Lookup(name) => {
-            let page = DocPage::fetch(false, &name);
-            match page {
-                Err(e) => println!("{:?}", e),
-                Ok(p) => p.print_tableview(),
-            }
-        },
+        Cmd::Lookup(name) => process_crate_fetch_cmds(false, &name),
         Cmd::InvalidUsage(s) => println!("Invalid command usage: {}", s),
-        Cmd::RefreshCache => {
-            println!("Refresh the doc cache? (y/n)");
-            println!("note: this may take a couple minutes depending on number of crates.");
-            let confirm = wait_for_cmd();
-            match Allow::from(confirm) {
-                Allow::Yes => {
-                    Command::new("cargo").arg("doc").spawn().expect("Failed to refresh docs");
-                },
-                Allow::No => return
-            }
-        },
+        Cmd::RefreshCache => process_refresh_cmd(),
+        _ => println!("Must lookup a crate before that cmd can be used")
     }
 }
 
+fn process_crate_fetch_cmds(online: bool, name: &str) {
+    loop {
+        let page = DocPage::fetch(online, &name);
+        match page {
+            Err(ContentError::DoesNotExist) if !online => {
+                let confirm = wait_for_cmd("Docs are missing - fetch live page? >>");
+                match Allow::from(confirm) {
+                    Allow::Yes => process_crate_fetch_cmds(true, name),
+                    Allow::No => break
+                }
+            }
+            Err(ContentError::DoesNotExist) => {
+                println!("Docs for {} cannot be found", &name);
+                break;
+            }
+            Err(ContentError::LoadFailure) if online => {
+                println!("Failed to fetch html, 
+                make sure you are connected to the internet.");
+                break;
+            },
+            Err(e) => println!("{:?}", e),
+            Ok(p) => process_opened_crate_cmds(p, &name),
+        }
+    }
+}
+
+fn process_opened_crate_cmds(p: DocPage, name: &str) {
+    // Here we have a valid doc page open.
+    // Enter a new state where we are looping
+    // cmds onto this doc page.
+    let state = DocState::from(p);
+    state.page.print_tableview();
+    loop {
+        let cmd_prmpt = format!("( {} ) >>", &name);
+        let cmd = Cmd::from(wait_for_cmd(&cmd_prmpt));
+        match cmd {
+            Cmd::Lookup(_) => process_cmd(cmd),
+            Cmd::Empty => continue,
+            Cmd::RefreshCache => process_refresh_cmd(),
+            Cmd::Doc(s) => {
+                match state.search_doc_listings(&s) {
+                    None => println!("Did not match any docs"),
+                    Some(d) => println!("{} {}", &d.name, &d.url),
+                }
+            }
+            _ => continue,
+        }
+    }
+}
+
+fn process_refresh_cmd() {
+    let confirm = wait_for_cmd("Refresh the doc cache? >>");
+    match Allow::from(confirm) {
+        Allow::Yes => {
+            Command::new("cargo").arg("doc").spawn().expect("Failed to refresh docs");
+        },
+        Allow::No => {
+            println!("Skipping refresh.");
+        }
+    }
+}
